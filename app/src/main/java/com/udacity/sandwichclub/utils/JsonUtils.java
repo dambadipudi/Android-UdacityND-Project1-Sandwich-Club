b2df870c4260e39df4d92bf66d3a99d331cea11e
dambadipudi <divya.shastry@gmail.com>
2018-04-27 16:37:34 -0700

    Added code to parse JSON into Model Object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,64 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+
+            JSONObject sandwichJSON = new JSONObject(json);
+
+            JSONObject nameJSON = sandwichJSON.getJSONObject("name");
+
+            String mainName = nameJSON.getString("mainName");
+
+            JSONArray alsoKnownAsJSONArray = nameJSON.getJSONArray("alsoKnownAs");
+
+            List<String> alsoKnownAsList = convertJSONArrayToListOfStrings(alsoKnownAsJSONArray);
+
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
+
+            String description = sandwichJSON.getString("description");
+
+            String image = sandwichJSON.getString("image");
+
+            JSONArray ingredientJSONArray = sandwichJSON.getJSONArray("ingredients");
+
+            List<String> ingredientList = convertJSONArrayToListOfStrings(ingredientJSONArray);
+
+            Sandwich sandwich = new Sandwich (mainName,
+                                              alsoKnownAsList,
+                                              placeOfOrigin,
+                                              description,
+                                              image,
+                                              ingredientList);
+
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<String> convertJSONArrayToListOfStrings(JSONArray jsonArray) throws JSONException {
+        List<String> listOfStrings = new ArrayList<>();
+
+        for(int index = 0; index < jsonArray.length(); index++) {
+            listOfStrings.add(jsonArray.getString(index));
+        }
+
+        return listOfStrings;
     }
 }
