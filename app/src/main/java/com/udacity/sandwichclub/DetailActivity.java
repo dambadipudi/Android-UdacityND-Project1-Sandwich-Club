4874b37c39383317eb078072a2d96f06066f5b8b
dambadipudi <divya.shastry@gmail.com>
2018-04-28 11:16:20 -0700

    Fixes #5: Populated all fields in the layout

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,41 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Added code to populate all the fields in the layout from the sandwich object
+     *
+     * @param sandwich
+     */
 
+    private void populateUI(Sandwich sandwich) {
+        // Populate the Also Known As information
+        TextView alsoKnownAsTextView = (TextView) findViewById(R.id.also_known_tv);
+        StringBuilder alsoKnownAsString = new StringBuilder();
+        for(String curAlsoKnownAs : sandwich.getAlsoKnownAs()) {
+            if(alsoKnownAsString.length() != 0) {
+                alsoKnownAsString.append("\n");
+            }
+            alsoKnownAsString.append(curAlsoKnownAs);
+        }
+        alsoKnownAsTextView.setText(alsoKnownAsString);
+
+        // Populate the Place of Origin information
+        TextView placeOfOriginTextView = (TextView) findViewById(R.id.origin_tv);
+        placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+
+        // Populate the Ingredients List information
+        TextView ingredientsTextView = (TextView) findViewById(R.id.ingredients_tv);
+        StringBuilder ingredientsString = new StringBuilder();
+        for(String ingredientString : sandwich.getIngredients()) {
+            if(ingredientsString.length() != 0) {
+                ingredientsString.append("\n");
+            }
+            ingredientsString.append(ingredientString);
+        }
+        ingredientsTextView.setText(ingredientsString);
+
+        // Populate the Description information
+        TextView descriptionTextView = (TextView) findViewById(R.id.description_tv);
+        descriptionTextView.setText(sandwich.getDescription());
     }
 }
