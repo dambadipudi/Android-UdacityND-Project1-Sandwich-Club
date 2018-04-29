2e7925818246ce01906c34aa5ccf61018aadb1f8
dambadipudi <divya.shastry@gmail.com>
2018-04-28 23:16:24 -0700

    Fixes #13: Added data binding

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
@@ -19,10 +21,14 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    //Create a data binding instance
+    ActivityDetailBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -84,7 +90,6 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI(Sandwich sandwich) {
         // Populate the Also Known As information
-        TextView alsoKnownAsTextView = (TextView) findViewById(R.id.also_known_tv);
         StringBuilder alsoKnownAsString = new StringBuilder();
         for(String curAlsoKnownAs : sandwich.getAlsoKnownAs()) {
             if(alsoKnownAsString.length() != 0) {
@@ -92,14 +97,12 @@ public class DetailActivity extends AppCompatActivity {
             }
             alsoKnownAsString.append(curAlsoKnownAs);
         }
-        alsoKnownAsTextView.setText(alsoKnownAsString);
+        mBinding.alsoKnownTv.setText(alsoKnownAsString);
 
         // Populate the Place of Origin information
-        TextView placeOfOriginTextView = (TextView) findViewById(R.id.origin_tv);
-        placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+        mBinding.originTv.setText(sandwich.getPlaceOfOrigin());
 
         // Populate the Ingredients List information
-        TextView ingredientsTextView = (TextView) findViewById(R.id.ingredients_tv);
         StringBuilder ingredientsString = new StringBuilder();
         for(String ingredientString : sandwich.getIngredients()) {
             if(ingredientsString.length() != 0) {
@@ -107,51 +110,50 @@ public class DetailActivity extends AppCompatActivity {
             }
             ingredientsString.append(ingredientString);
         }
-        ingredientsTextView.setText(ingredientsString);
+        mBinding.ingredientsTv.setText(ingredientsString);
 
         // Populate the Description information
-        TextView descriptionTextView = (TextView) findViewById(R.id.description_tv);
-        descriptionTextView.setText(sandwich.getDescription());
+        mBinding.descriptionTv.setText(sandwich.getDescription());
     }
 
     /**
      * This method is called when the Also Known As label is clicked
      */
     public void onAlsoKnownLabelClicked(View view) {
-        ImageView down_arrow = findViewById(R.id.also_known_as_down_arrow);
-        ImageView up_arrow = findViewById(R.id.also_known_as_up_arrow);
-        TextView informationTextView = findViewById(R.id.also_known_tv);
-        toggleFields(view, down_arrow, up_arrow, informationTextView);
+        toggleFields(view,
+                    mBinding.alsoKnownAsDownArrow,
+                    mBinding.alsoKnownAsUpArrow,
+                    mBinding.alsoKnownTv);
     }
 
     /**
      * This method is called when the Place of Origin label is clicked
      */
     public void onOriginClicked(View view) {
-        ImageView down_arrow = findViewById(R.id.place_of_origin_down_arrow);
-        ImageView up_arrow = findViewById(R.id.place_of_origin_up_arrow);
-        TextView informationTextView = findViewById(R.id.origin_tv);
-        toggleFields(view, down_arrow, up_arrow, informationTextView);
+        toggleFields(view,
+                    mBinding.placeOfOriginDownArrow,
+                    mBinding.placeOfOriginUpArrow,
+                    mBinding.originTv);
     }
 
     /**
      * This method is called when the Ingredients label is clicked
      */
     public void onIngredientsClicked(View view) {
-        ImageView down_arrow = findViewById(R.id.ingredients_down_arrow);
-        ImageView up_arrow = findViewById(R.id.ingredients_up_arrow);
-        TextView informationTextView = findViewById(R.id.ingredients_tv);
-        toggleFields(view, down_arrow, up_arrow, informationTextView);
+        toggleFields(view,
+                    mBinding.ingredientsDownArrow,
+                    mBinding.ingredientsUpArrow,
+                    mBinding.ingredientsTv);
     }
 
     /**
      * This method is called when the Description label is clicked
      */
     public void onDescriptionClicked(View view) {
-        ImageView down_arrow = findViewById(R.id.description_down_arrow);
-        ImageView up_arrow = findViewById(R.id.description_up_arrow);
-        TextView informationTextView = findViewById(R.id.description_tv);
-        toggleFields(view, down_arrow, up_arrow, informationTextView);
+        toggleFields(view,
+                    mBinding.descriptionDownArrow,
+                    mBinding.descriptionUpArrow,
+                    mBinding.descriptionTv);
     }
 
     /**
